9d77599a630cb1308a5f83329c13d29d3b2edf48
Awais <d.buccolieri.esterno@almaviva.it>
2024-07-08 11:42:42 +0200

    Update App.js to reflect successful Ansible 2 deployment and modify link text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ansible Deployment with React App Worked Successfully!
+          Ansible 2 Deployment with React App Worked Successfully!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TEST Link
         </a>
       </header>
     </div>
